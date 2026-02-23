c39f58786e92df2457d8baabb30f87d9639e28fb
Soskiiit <NikitaA439@gmail.com>
2026-02-23 23:09:34 +0300

    modified:   .gitignore modified:   LAB_4 Tests.postman_collection.json modified:   src/dogs/dto/update-dog.dto.ts

--- src/dogs/dto/update-dog.dto.ts
+++ src/dogs/dto/update-dog.dto.ts
@@ -1,7 +1,7 @@
 import { CreateDogAccordionItemDto } from './create-dog.dto';
 
 export class UpdateDogDto {
-  src?: string;
+  image_src?: string;
   title?: string;
   text?: string;
   accordionData?: CreateDogAccordionItemDto[];
