bbedcd55e7fbae0968d09f54d590f34f1f2f4aac
Soskiiit <NikitaA439@gmail.com>
2026-02-24 16:23:06 +0300

    Now it serves all

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { resolve } from 'path';
+
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  app.useStaticAssets(resolve(__dirname, '..', 'public'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
