05ce94b799de5da3b2e02ff973683ed559a7de30
Soskiiit <NikitaA439@gmail.com>
2026-02-27 13:25:44 +0300

    refactoring, minor UI changes

--- script.js
+++ script.js
@@ -1,119 +1,117 @@
 const darkModeMql = window.matchMedia('(prefers-color-scheme: dark)');
-if (darkModeMql.matches) 
+if (darkModeMql.matches)
     document.body.classList.toggle('dark-theme');
 
+let first_number = '';
+let second_number = '';
+let expressionResult = '';
+let selectedOperation = null;
 
-let a = ''
-let b = ''
-let expressionResult = ''
-let selectedOperation = null
+const outputElement = document.getElementById("result");
 
-const outputElement = document.getElementById("result")
-
-const digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+const digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]');
 
 function onDigitButtonClicked(digit) {
     if (!selectedOperation) {
-        if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
-            a += digit;
+        if ((digit !== '.') || (digit === '.' && !first_number.includes('.'))) {
+            first_number += digit;
         }
-        outputElement.innerHTML = a;
-    } else {
-        if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
-            b += digit;
-            outputElement.innerHTML = b;        
-        }
+        outputElement.innerHTML = first_number;
+    } else if ((digit !== '.') || (digit === '.' && !second_number.includes('.'))) {
+        second_number += digit;
+        outputElement.innerHTML = second_number;
     }
+
 }
 
 digitButtons.forEach(button => {
-    button.onclick = function() {
+    button.onclick = function () {
         const digitValue = button.innerHTML;
         onDigitButtonClicked(digitValue);
-    }
+    };
 });
 
-document.getElementById("btn_op_mult").onclick = function() { 
-    if (a === '') a = '0';
+document.getElementById("btn_op_mult").onclick = function () {
+    if (first_number === '') first_number = '0';
     selectedOperation = 'x';
-}
-document.getElementById("btn_op_plus").onclick = function() { 
-    if (a === '') a = '0';
+};
+document.getElementById("btn_op_plus").onclick = function () {
+    if (first_number === '') first_number = '0';
     selectedOperation = '+';
-}
-document.getElementById("btn_op_minus").onclick = function() { 
-    if (a === '') a = '0';
+};
+document.getElementById("btn_op_minus").onclick = function () {
+    if (first_number === '') first_number = '0';
     selectedOperation = '-';
-}
-document.getElementById("btn_op_div").onclick = function() { 
-    if (a === '') a = '0';
+};
+document.getElementById("btn_op_div").onclick = function () {
+    if (first_number === '') first_number = '0';
     selectedOperation = '/';
-}
+};
 
-document.getElementById("btn_op_clear").onclick = function() { 
-    a = ''
-    b = ''
-    selectedOperation = ''
-    expressionResult = ''
-    outputElement.innerHTML = 0
-}
+document.getElementById("btn_op_clear").onclick = function () {
+    first_number = '';
+    second_number = '';
+    selectedOperation = '';
+    expressionResult = '';
+    outputElement.innerHTML = 0;
+};
 
-document.getElementById("btn_op_sign").onclick = function() {
+document.getElementById("btn_op_sign").onclick = function () {
     if (!selectedOperation) {
-        a = (-a).toString();
-        outputElement.innerHTML = a;
-        if (a == 0)
-            a = '';
+        first_number = (-first_number).toString();
+        outputElement.innerHTML = first_number;
+        if (first_number == 0)
+            first_number = '';
     } else {
-        b = (-b).toString();
-        outputElement.innerHTML = b;
-        if (b == 0)
-            b = '';
+        second_number = (-second_number).toString();
+        outputElement.innerHTML = second_number;
+        if (second_number == 0)
+            second_number = '';
     }
-}
+};
 
-document.getElementById("btn_op_backspace").onclick = function() {
+document.getElementById("btn_op_backspace").onclick = function () {
     if (!selectedOperation) {
-        a = a.toString().slice(0, -1);
-        if (a)
-            outputElement.innerHTML = a;
+        first_number = first_number.toString().slice(0, -1);
+        if (first_number)
+            outputElement.innerHTML = first_number;
         else
             outputElement.innerHTML = 0;
     } else {
-        b = b.toString().slice(0, -1);
-        if (b)
-            outputElement.innerHTML = b;
+        second_number = second_number.toString().slice(0, -1);
+        if (second_number)
+            outputElement.innerHTML = second_number;
         else
             outputElement.innerHTML = 0;
     }
-}
+};
 
-document.getElementById("btn_theme").onclick = function() {
+document.getElementById("btn_theme").onclick = function () {
     document.body.classList.toggle('dark-theme');
-}
+};
 
-document.getElementById("btn_op_equal").onclick = function() { 
-    if (a === '' || b === '' || !selectedOperation)
-        return
-        
-    switch(selectedOperation) { 
+document.getElementById("btn_op_equal").onclick = function () {
+    if (first_number === '' || second_number === '' || !selectedOperation)
+        return;
+
+    switch (selectedOperation) {
         case 'x':
-            expressionResult = (+a) * (+b)
+            expressionResult = (+first_number) * (+second_number);
             break;
         case '+':
-            expressionResult = (+a) + (+b)
+            expressionResult = (+first_number) + (+second_number);
             break;
         case '-':
-            expressionResult = (+a) - (+b)
+            expressionResult = (+first_number) - (+second_number);
             break;
         case '/':
-            expressionResult = +((+a) / (+b)).toFixed(6)
+            expressionResult = +((+first_number) / (+second_number)).toFixed(6);
             break;
     }
-    
-    a = expressionResult.toString()
-    b = ''
-    selectedOperation = null
 
-    outputElement.innerHTML = a
-}
+    first_number = expressionResult.toString();
+    second_number = '';
+    selectedOperation = null;
+
+    outputElement.innerHTML = first_number;
+};
